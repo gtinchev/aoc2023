c82c37fbabbb0fd4087a9cc1556ac5d7435fe8ec
Georgi Tinchev <georgi@tinchev.net>
2023-12-14 13:08:39 +0200

    Day 9: Parts 1 and 2 - done.

---
+++ day9/part2.py
@@ -0,0 +1,47 @@
+#/usr/bin/env python3
+""" Day 9: Mirage Maintenance 
+
+Part 1: This reminds me of the traingle of differences, and that you can estimate any number on any given scale of that triangle.
+But I can't remember for the life of me what the formula was.
+Let's do it the naive way, and perhaps I can come up with the formula in the meantime.
+
+Part 2: It's the same but instead of the sum of the differences of the last digit, it's the difference or sum (depending on the row) of the first digits in each row of differences.
+"""
+
+import argparse
+import numpy as np
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process input file')
+    parser.add_argument('--input_file', type=str, help='Path to the input file')
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+    final_sum = 0
+    with open(args.input_file, "r") as file:
+        for line in file:
+            numbers = np.array([int(x) for x in line.strip().split()], dtype=np.int64)
+            found = False
+            first_digits = numbers[0]
+            difference_of_differences = 0
+            idx = 0
+            while not found:
+                differences = np.diff(numbers)
+                # check if the row is even and add the first digit, otherwise subtract it
+                if idx % 2 == 0:
+                    difference_of_differences += differences[0]
+                else:
+                    difference_of_differences -= differences[0]
+                if all(x == differences[0] for x in differences):
+                    found = True
+                    final_sum += (first_digits - difference_of_differences)
+
+                idx += 1
+                numbers = differences
+
+    print("Final sum: {}".format(final_sum))
+
+if __name__ == '__main__':
+     main()
